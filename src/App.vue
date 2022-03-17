01fe49adf4478778f08c789516bf2e57763cfb23
Kauanphbbb <kauanphbbb@gmail.com>
2022-03-17 16:14:36 -0300

    feat: css for title and stickes options

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,20 @@
 <template>
+<div id="app">
   <RouterView />
+  </div>
 </template>
 
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+#app {
+  min-height: 100vh;
+  min-width: 100vh;
+  font-family: Arial, Helvetica, sans-serif;
+  background-color: rgb(16, 6, 105);
+  box-sizing: border-box;
+  padding-top: 10px;
+}
+</style>>
--- src/components/FormComponent.vue
+++ src/components/FormComponent.vue
@@ -15,31 +15,76 @@ export default {
 </script>
 
 <template>
-  <div class="title-form">
-    <h3>Formulário para compra de <strong> Pacote de adesivos </strong></h3>
-  </div>
-
-  <form>
-    <div class="stickers-options">
-      <p>Selecione seus adesivos:</p>
-      <div v-for="(sticker, index) in stickersOptions" :key="index">
-        <input type="checkbox" :name="sticker" :id="sticker" />
-        <label :for="sticker">{{ sticker }}</label>
-      </div>
+  <div class="form-container">
+    <div class="title-form">
+      <p>Formulário para compra de <strong> Pacote de adesivos </strong></p>
     </div>
 
-    <div class="stickers-qtd">
-      <p>Quantos adesivos de cada?</p>
-      <div class="sticker-qtd-input">
-        <button>-</button>
-        <input type="number" name="stickersQTD" v-model="form.stickersQTD" />
-        <button>+</button>
+    <form>
+      <div class="stickers-options margin-top">
+        <p>Selecione seus adesivos:</p>
+        <div class="sticker" v-for="(sticker, index) in stickersOptions" :key="index">
+          <input type="checkbox" :name="sticker" :id="sticker" />
+          <label :for="sticker">{{ sticker }}</label>
+        </div>
       </div>
-    </div>
 
-    <div class="message">
+      <div class="stickers-qtd margin-top">
+        <p>Quantos adesivos de cada?</p>
+        <div class="sticker-qtd-input">
+          <button>-</button>
+          <input readonly type="number" name="stickersQTD" v-model="form.stickersQTD" />
+          <button>+</button>
+        </div>
+      </div>
+
+      <div class="message margin-top">
         <p>Observação:</p>
-        <textarea name="message" v-model="form.message" cols="50" rows="10"></textarea>
-    </div>
-  </form>
+        <textarea
+          name="message"
+          v-model="form.message"
+          cols="40"
+          rows="10"
+          placeholder="Alguma dúvida? Deixe aqui!"
+        ></textarea>
+      </div>
+
+      <div class="button=container margin-top">
+        <button type="submit">Enviar</button>
+      </div>
+    </form>
+  </div>
 </template>
+
+<style scoped>
+.form-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 400px;
+  height: 100%;
+  margin: 0 auto;
+  box-sizing: border-box;
+  background-color: rgb(71, 68, 68);
+  color: rgb(255, 255, 255);
+}
+
+.title-form {
+    background-color: rgb(73, 11, 131);
+    padding: 20px;
+    font-size: 24px;
+}
+
+.sticker {
+  margin-top: 10px;
+}
+
+.sticker input {
+  margin-right: 10px;
+}
+
+.margin-top {
+  margin-top: 20px;
+}
+</style>
