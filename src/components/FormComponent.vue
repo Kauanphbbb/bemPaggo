d27b786a92ffe5a266503171183b4ffe4776e871
Kauanphbbb <kauanphbbb@gmail.com>
2022-03-17 15:38:26 -0300

    feat: montagem da estrutura com html

---
+++ src/components/FormComponent.vue
@@ -0,0 +1,45 @@
+<script>
+export default {
+  name: "FormComponent",
+  data() {
+    return {
+      form: {
+        stickers: [],
+        stickersQTD: 0,
+        message: "",
+      },
+      stickersOptions: ["React", "Vue", "Angular"],
+    };
+  },
+};
+</script>
+
+<template>
+  <div class="title-form">
+    <h3>Formulário para compra de <strong> Pacote de adesivos </strong></h3>
+  </div>
+
+  <form>
+    <div class="stickers-options">
+      <p>Selecione seus adesivos:</p>
+      <div v-for="(sticker, index) in stickersOptions" :key="index">
+        <input type="checkbox" :name="sticker" :id="sticker" />
+        <label :for="sticker">{{ sticker }}</label>
+      </div>
+    </div>
+
+    <div class="stickers-qtd">
+      <p>Quantos adesivos de cada?</p>
+      <div class="sticker-qtd-input">
+        <button>-</button>
+        <input type="number" name="stickersQTD" v-model="form.stickersQTD" />
+        <button>+</button>
+      </div>
+    </div>
+
+    <div class="message">
+        <p>Observação:</p>
+        <textarea name="message" v-model="form.message" cols="50" rows="10"></textarea>
+    </div>
+  </form>
+</template>
---
+++ src/views/FormView.vue
@@ -0,0 +1,13 @@
+<script>
+import FormComponent from "@/components/FormComponent.vue";
+export default {
+    name: "FormView",
+    components: {
+        FormComponent
+    },
+}
+</script>
+
+<template>
+    <FormComponent/>
+</template>
